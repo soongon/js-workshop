89be76937f66451d74130a0c186c4b2394262218
soongon <soongon@hucloud.co.kr>
2021-11-09 10:25:31 +0900

    initial commit

---
+++ nodeArrowFunction.js
@@ -0,0 +1,7 @@
+const addTwoNumbers = function (numOne, numTwo) {
+  return numOne + numTwo;
+};
+
+// 익명함수는 화살표 함수로 변환할 수 있다.
+const addTwoNumbers2 = (numOne=0, numTwo=0) => numOne + numTwo;
+
---
+++ nodeIIFE.js
@@ -0,0 +1,6 @@
+const result2 = ((numOne, numTwo) => numOne + numTwo)(3, 2);
+
+// const result = addTwoNumbers(3, 5);
+// console.log(result);
+
+console.log(result2);
